f92889200f67989623efc9183686b60869650f66
Anaziiii <kavaansraaiia@gmail.com>
2026-03-21 23:57:41 +0200

    fix

--- anastasiia-kava/lab6/lab6.py
+++ anastasiia-kava/lab6/lab6.py
@@ -1,13 +1,31 @@
-class Dog:
+class Phone:
+    how_many = 0
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.price_in_dollars = self.price / 42
+        Phone.how_many += 1
+
+    def ring(self):
+        return f"{self.brand} : Beep! Beep!"
     
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def info(self):
+        return f"brand: {self.brand} model: {self.model} price: {self.price} price in dollars: {self.price_in_dollars}"
+    
+    @classmethod
+    def count(Phone):
+        return Phone.how_many
 
-    def bark(self):
-        return "Woof!"
-my_dog = Dog("Buddy", 3)
+phone1 = Phone("Apple", "iPhone", 6000)
+phone2 = Phone("Samsung", "Galaxy", 5000)
 
-print(my_dog.name) 
-print(my_dog.age)
-print(my_dog.bark())
+print(Phone.count())
+
+print(Phone.ring(phone2))
+print(phone2.ring())
+
+print(Phone.info(phone1))
+print(phone1.info())
+
+
